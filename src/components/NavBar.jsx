e1a756679814f77da1b6a741db5b292a072cb0b8
KayGV <khuongvudang25@gmail.com>
2024-06-14 13:15:03 +0700

    [Update] dark mode toggle

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,7 +18,7 @@ export default function NavBar() {
       <nav
         className={`bg-slate-800 h-16 flex justify-between align-middle ${
           checkMobile ? "px-3" : "px-20"
-        } w-screen pt-3 sticky text-gray-50 fixed top-0`}
+        } w-screen pt-3 sticky text-gray-50 top-0`}
       >
         {checkMobile ? 
                 <>
@@ -64,7 +64,7 @@ function DarkModeSwitch(){
     return(
         <>
             <label className="inline-flex items-center cursor-pointer">
-                <input type="checkbox" checked={!isDark} onClick={() => {useDarkMode(); setIsDark(!isDark)}} className="sr-only peer outline-none"/>
+                <input type="checkbox" checked={isDark} onChange={() => {useDarkMode(); setIsDark(!isDark)}} className="sr-only peer outline-none"/>
                 <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-900"></div>
             </label>
         </>
