08ed1cc2733c6caa8ad930ca7a754b4d75033e1d
KayGV <khuongvudang25@gmail.com>
2024-07-10 15:52:35 +0700

    [UPDATE] gitignore

--- src/hooks/isMobile.jsx
+++ src/hooks/isMobile.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 
 export default function isMobile(){
     const [Check, setCheck] = useState(true);
+    const HW = 620;
 
     window.addEventListener('load', () =>{
-        setCheck(window.innerWidth <= 500 || window.innerHeight <= 500);
+        setCheck(window.innerWidth <= HW || window.innerHeight <= HW - 200);
     })
     window.addEventListener('resize', () =>{
-        setCheck(window.innerWidth <= 500 || window.innerHeight <= 500);
+        setCheck(window.innerWidth <= HW || window.innerHeight <= HW - 200);
     })
     return Check;
 }
