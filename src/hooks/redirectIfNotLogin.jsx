4efa93993b89ee7543a5631bace33d993ef0f893
KayGV <khuongvudang25@gmail.com>
2024-07-25 16:52:40 +0700

    [UPDATE] login redirect

--- src/hooks/redirectIfNotLogin.jsx
+++ src/hooks/redirectIfNotLogin.jsx
@@ -1,9 +1,11 @@
 export function redirectIfNotLogin(){
-    let username = window.sessionStorage.getItem('username');
-    if(username != null ){
-        console.log("ok")
-    }
-    else {
-        return window.location.href = "/login";  // Redirect to login page if user is not logged in.
+    window.onload = function(){
+        let username = window.sessionStorage.getItem('username');
+        if(username != null ){
+            console.log("ok")
+        }
+        else {
+            return window.location.href = "/login";  // Redirect to login page if user is not logged in.
+        }
     }
 }
