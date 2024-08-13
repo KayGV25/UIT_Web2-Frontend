f0870e39e104061776c2dd3fdc741e3f1da9d3ba
KayGV <khuongvudang25@gmail.com>
2024-08-13 08:41:55 +0700

    [UPDATE] add shadow for login and signup page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -47,7 +47,7 @@ export default function SignUpPage() {
         return(
             <div className="h-dvh grid">
                 <div className=" flex justify-center align-middle my-auto">
-                    <div className="bg-zinc-100 rounded-lg px-20 py-20 text-gray-900 min-h-96 h-auto">
+                    <div className="bg-zinc-100 shadow-md rounded-lg px-20 py-20 text-gray-900 min-h-96 h-auto">
                         <h1 className="text-center text-3xl font-bold">Sign up</h1>
                         <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col">
                             <CustomInput name="Username" type="text" value={newUsername}/>
